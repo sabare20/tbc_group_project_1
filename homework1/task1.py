74f05db38f6a5bee746c5d80edf38d0f82bf424c
sabare20 <phirtskhalaishvilisaba@gmail.com>
2024-09-14 02:04:31 +0400

    second comite(changed)

---
+++ homework1/task1.py
@@ -0,0 +1,3 @@
+name=input('enter your first name and lastname :')
+age=input('enter your age :')
+print('hi,'+name+' you are '+age+' years old.')
---
+++ homework1/task2.py
@@ -0,0 +1,4 @@
+name=input('emter your name :')
+birthday_year=int(input('enter your birth year :'))
+age =2024-birthday_year
+print('hi ,'+name+' you are '+str(age)+' years old.')
