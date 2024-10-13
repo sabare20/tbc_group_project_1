bbb4d3c3d285c17308005c8866fe8547f5e0bb47
sabare20 <phirtskhalaishvilisaba@gmail.com>
2024-10-13 21:27:53 +0400

    task 5 done

--- homework6/task_4.py
+++ homework6/task_4.py
@@ -1,6 +1,6 @@
-n=5
-x=-4
-y=1
+n=int(input('enter number form 1 to 10 :'))
+x=-n+1
+
 while n-abs(x)>0:
     i=1
     while n-abs(x)-i>=0:
--- homework6/task_5.py
+++ homework6/task_5.py
@@ -1,16 +1,14 @@
 input_num=int(input('enter number from 1 to 10 :'))
 x=input_num
 z=0
-
 while input_num-z>=0:
     y=0
     while input_num-z-y>0:
         print(' ',end='')
         y+=1
     m=0
-    n=0
-    while abs(-m)<=m and m<=input_num:
-        print(abs(-m),end='')
+    while abs(z-m)<=z :
+        print(abs(z-m),end='')
         m+=1
     print()
     z+=1
