9cab18a3a484842be4898e34ef44312320b837b4
sabare20 <phirtskhalaishvilisaba@gmail.com>
2024-12-06 15:01:55 +0400

    corrected hmw18

--- homework18/task_1.py
+++ homework18/task_1.py
@@ -1,9 +1,10 @@
-with open('homework18/data.txt','r') as file:
-    with open('homework18/high.txt','w') as high , open('homework18/small.txt','w') as small:
+with open('homework18/data.txt', 'r') as file:
+    with open('homework18/high.txt', 'w') as high, open('homework18/small.txt', 'w') as small:
         for line in file:
-            line=line.strip
-            value = float(line.split(',')[2])*float(line.split(',')[3])
-            if  value >= 10:
+            line = line.strip()
+            parts = line.split(',')
+            value = float(parts[2]) * float(parts[3])
+            if value >= 10:
                 high.write(f'{line}\n')
-            elif value < 10:
+            else:
                 small.write(f'{line}\n')
